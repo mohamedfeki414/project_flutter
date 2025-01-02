a5efcba63629dfe41e47992fe32af706552ae943
feki_mohamed <medfeki33@gmail.com>
2025-01-02 18:11:43 +0100

    ajouter main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,160 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FootballLegendsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FootballLegendsApp extends StatelessWidget {
+  const FootballLegendsApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Football Legends',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/teams': (context) => const TeamsScreen(),
+        '/players': (context) => const PlayersScreen(),
+        '/leagues': (context) => const LeaguesScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('Connexion')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            const TextField(
+              decoration: InputDecoration(labelText: 'Email'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: const Text('Se connecter'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Mot de passe oublié ?'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Menu Principal')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/players');
+              },
+              child: const Text('Top 3 des joueurs'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/teams');
+              },
+              child: const Text('Équipes légendaires'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/leagues');
+              },
+              child: const Text('Top 5 des ligues'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TeamsScreen extends StatelessWidget {
+  const TeamsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> teams = ['FC Barcelone', 'Real Madrid', 'Bayern Munich'];
+    return Scaffold(
+      appBar: AppBar(title: const Text('Équipes Légendaires')),
+      body: ListView.builder(
+        itemCount: teams.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(teams[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PlayersScreen extends StatelessWidget {
+  const PlayersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> players = ['Lionel Messi', 'Diego Maradona', 'Pelé'];
+    return Scaffold(
+      appBar: AppBar(title: const Text('Top 3 Joueurs')),
+      body: ListView.builder(
+        itemCount: players.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(players[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LeaguesScreen extends StatelessWidget {
+  const LeaguesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> leagues = ['Premier League', 'Serie A', 'La Liga'];
+    return Scaffold(
+      appBar: AppBar(title: const Text('Top 5 Ligues')),
+      body: ListView.builder(
+        itemCount: leagues.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(leagues[index]),
+          );
+        },
+      ),
     );
   }
 }
